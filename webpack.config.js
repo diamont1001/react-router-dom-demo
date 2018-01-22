f3292287706c01ca542e79f485facc8bc7af64f4
材主 <diamont1001@163.com>
2018-01-22 20:10:13 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   devtool: 'eval-source-map',
 
-  entry:  __dirname + "/app/main.js",
+  // entry:  __dirname + "/app/main.js",
+  entry:  {
+    main: __dirname + "/app/main.js"
+  },
   output: {
     path: __dirname + "/build",
     filename: "bundle.js"
